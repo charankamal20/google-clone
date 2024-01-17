b80750701999a251e69a15ea1f0ffd8a397557bb
charankamal20 <charanksingh20@gmail.com>
2024-01-18 01:16:18 +0530

    Handled errors for search

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,9 +1,22 @@
+import Link from 'next/link'
 
 async function WebSearchPage({searchParams}) {
   const response = await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`);
   const data = await response.json();
   const results = data.items;
 
+
+
+  if(!results) {
+    return <div className="w-full pt-10 flex flex-col justify-center items-center"> 
+      <h1 className="text-lg">No result found</h1>
+        <p>Try searching for something else or go back to homepage.</p>
+        <Link className="text-blue-500" href="/">
+          Home
+        </Link>
+    </div>
+  }
+  
   return (
     <>
       {results && results.map((result, index) => 
