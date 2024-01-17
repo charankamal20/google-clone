61afc32bfbbcfc50578d582d391549ce8ad3dd8a
charankamal20 <charanksingh20@gmail.com>
2024-01-18 00:53:55 +0530

    Implement Search with api

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
 
-function WebSearchPage() {
+async function WebSearchPage({searchParams}) {
+  const response = await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`);
+  const data = await response.json();
+  const results = data.items;
+
   return (
-    <div>WebSearchPage</div>
-  )
+    <>
+      {results && results.map((result, index) => 
+        <h1 key={index}>{result.title}</h1>
+      )}
+    </>
+  );
 }
 
 export default WebSearchPage;
